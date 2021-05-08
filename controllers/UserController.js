3e327cfa4e1795f5936bbde0f1744d14916a2cbb
Janghyeon Lee <janghyeon300@gmail.com>
2021-05-08 22:00:25 +0900

    wip

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,6 +5,53 @@ const jwt = require('jsonwebtoken');
 const { UserService } = require('../services');
 
 const signUp = async (req, res, next) => {
+  validateId = (id) => {
+    const validIdRegExp = /\w@\w+\.\w/i;
+    return id.match(validIdRegExp) ? true : false;
+  };
+
+  validatePw = (pw, setErrorMsg = true) => {
+    const pwValidation = {
+      regexUppercase: /[A-Z]/g,
+      regexLowercase: /[a-z]/g,
+      regexSpecialCharacter: /[!|@|#|$|%|^|&|*]/g,
+      regexDigit: /[0-9]/g,
+    };
+    const errMsgMatch = {
+      regexUppercase: '영어 대문자를',
+      regexLowercase: '영어 소문자를',
+      regexSpecialCharacter: '특수 문자를',
+      regexDigit: '숫자를',
+    };
+    const MIN_PW_LENGTH = 8;
+    const pwLength = pw.length;
+
+    if (setErrorMsg && !pwLength) {
+      this.setState({ errorMessage: '' });
+      return false;
+    }
+
+    for (let validType in pwValidation) {
+      if (!pw.match(pwValidation[validType]) && pwLength) {
+        setErrorMsg &&
+          this.setState({
+            errorMessage: `비밀번호는 ${errMsgMatch[validType]} 포함해야 합니다.`,
+          });
+        return;
+        //let h = setErrorMsg? this.setState({errorMessage: `비밀번호는 ${errMsgMatch[validType]} 포함해야 합니다.`,}) : false
+      }
+    }
+
+    if (pwLength >= MIN_PW_LENGTH) return true;
+    else {
+      setErrorMsg &&
+        this.setState({
+          errorMessage: pwLength ? '비밀번호는 8자리 이상입니다.' : '',
+        });
+      return false;
+    }
+  };
+
   try {
     const {
       email,

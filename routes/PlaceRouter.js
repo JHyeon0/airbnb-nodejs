f1996fe49215df023e26e89956777277c6a794f6
soheon-lee <lsheon93@gmail.com>
2021-05-09 16:07:09 +0900

    Add: findOnePlaceDetail for host enabling to see detail information of her house

--- routes/PlaceRouter.js
+++ routes/PlaceRouter.js
@@ -5,5 +5,7 @@ const { PlaceController } = require('../controllers');
 // Route 는 오직 Controller 에만 의존 합니다.
 
 router.get('', PlaceController.placeList);
+router.get('/:placeId', PlaceController.findOnePlace);
+router.get('/:placeId/host', PlaceController.findOnePlaceDetail);
 
 module.exports = router; // 이렇게 내보내면 부모 router 에 자동으로 연결됩니다.

27754b14aa3ccf354a8cd996a0fb2a19f6783600
Janghyeon Lee <janghyeon300@gmail.com>
2021-05-10 20:26:25 +0900

    Modify: validateUser가 필요한 정보만 req에 담도록 수정

--- services/UserService.js
+++ services/UserService.js
@@ -1,9 +1,11 @@
 const prisma = require('../prisma'); // Service 로직은 오직 Model(=Prisma)에만 의존합니다.
 
+// user 생성
 const createUser = (field) => {
   return prisma.user.create({ data: field });
 };
 
+// user table에서 field로 필요한 데이터(selectFieldsArr)만 찾기.
 const findUser = (field, selectFieldsArr) => {
   const [uniqueKey] = Object.keys(field);
   const selectFields =
@@ -21,6 +23,7 @@ const findUser = (field, selectFieldsArr) => {
   });
 };
 
+// hostId를 반환. host가 아니면 null.
 const verifyHost = async (userId) => {
   const hostId = await prisma.$queryRaw(
     `SELECT id FROM Host WHERE userId=${userId}`,
